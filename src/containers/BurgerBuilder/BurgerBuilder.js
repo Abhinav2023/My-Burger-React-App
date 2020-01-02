fddba06f971af11736d565cee2726cfbc206df49
Abhinav2023 <abhinavbansal231101@gmail.com>
2020-01-02 10:19:31 +0530

    Creating MyBurger Builder App

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+import Aux from '../../hoc/Aux';
+
+class BuregerBuilder extends Component {
+    render(){
+        return (
+            <Aux>
+                <div></div>
+            </Aux>
+        )
+    }
+}
